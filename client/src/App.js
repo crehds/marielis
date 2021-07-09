6cf21cbf31c1f80ddabaf86e9a1277bfb6b7d54a
Unknown <carlos15erh@gmail.com>
2021-07-09 18:01:15 -0500

    carosuel with animation, arrows,autoplay
    not completed yet

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { GlobalStyle } from './GlobalStyles';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyle />
       <NavBar />
+      <Home />
+      {/* <div>Holi</div> */}
     </div>
   );
 }
---
+++ client/src/components/Carousel/index.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import NukaCarousel from 'nuka-carousel';
+import mario from '../../assets/mario_pastel.jpg';
+import tartaleta from '../../assets/tartaleta_fresa.jpg';
+import cristal from '../../assets/torta_cristal.jpg';
+import decorada from '../../assets/torta_decorada.jpg';
+import universitario from '../../assets/torta_u.jpg';
+import { CarouselWrapper } from './styles';
+
+export const Carousel = () => {
+  return (
+    <CarouselWrapper>
+      <NukaCarousel
+        autoplay
+        slideWidth='600px'
+        width='600px'
+        height='100%'
+        transitionMode='scroll3d'
+        pauseOnHover={false}
+        defaultControlsConfig={{
+          pagingDotsStyle: {
+            padding: '0px 8px'
+          }
+        }}
+        getControlsContainerStyles={(key) => {
+          switch (key) {
+            case 'CenterLeft':
+              return {
+                top: '50%',
+                left: '-80px'
+              };
+            case 'CenterRight':
+              return {
+                top: '50%',
+                right: '-80px'
+              };
+            default:
+              return {};
+          }
+        }}
+        renderCenterLeftControls={({ previousSlide }) => (
+          <div className='inicio-arrow'>
+            <i className='icon-keyboard_arrow_left' onClick={previousSlide}></i>
+          </div>
+        )}
+        renderCenterRightControls={({ nextSlide }) => (
+          <div className='inicio-arrow'>
+            <i className='icon-keyboard_arrow_right' onClick={nextSlide}></i>
+          </div>
+        )}
+      >
+        <img src={mario} alt='torta de mario' />
+        <img src={tartaleta} alt='tartaleta de fresa' />
+        <img src={cristal} alt='torta de cristal' />
+        <img src={decorada} alt='torta decorada' />
+        <img src={universitario} alt='torta de universitario' />
+      </NukaCarousel>
+    </CarouselWrapper>
+  );
+};
