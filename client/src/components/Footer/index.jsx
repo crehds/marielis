2a4e755225b372937afa0cb9378808ae2e6489ce
Unknown <carlos15erh@gmail.com>
2021-07-15 18:28:45 -0500

    Adding anchor for more info to contact the businness

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -15,15 +15,21 @@ export const Footer = () => {
           'Mz. D lote 16 Las Mercedes III etapa',
           'Lunes a Viernes 08:00 a 17:00 hrs'
         ]}
-        icons={[]}
+        icons={['location_on', '', 'access_time']}
+        links={['https://goo.gl/maps/RmFpfGiSczp3bEWt6', '', '']}
       />
       <ArticleInfo
         title={'Contáctanos'}
-        icons={['whatsapp', 'facebook-square', 'instagram', 'envelope']}
+        icons={['facebook-square', 'instagram', 'whatsapp', 'envelope']}
+        links={[
+          'https://www.facebook.com/Marielis-112533103834672',
+          'https://www.instagram.com/ma_rielis/',
+          ''
+        ]}
         content={[
-          '981680869',
           "marieli's",
           'ma_rielis',
+          '981680869',
           'marieliscakess@gmail.com'
         ]}
       />
