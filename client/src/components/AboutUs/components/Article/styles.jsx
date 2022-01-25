de7c5cbed4cd8c0419e7faafbf381f00bfd1c7a9
carlos rodriguez <carlos15erh@gmail.com>
2022-01-25 13:21:03 -0500

    some styles for articles(about us)

--- client/src/components/AboutUs/components/Article/styles.jsx
+++ client/src/components/AboutUs/components/Article/styles.jsx
@@ -1,13 +1,15 @@
 import styled from "styled-components"
 
 export const ArticleWrapper = styled.article`
-  border: 1px solid white;
+  /* border: 1px solid white; */
   display: grid;
   grid-template-rows: 15em 2fr 3fr;
+  box-shadow: 0px 0px 3px 1px rgba(0,0,0,0.4);
+  margin: 2px;
 `
 
 export const ArticleTitle = styled.div`
-  color: gray;
+  color: rgba(235, 177, 213, 0.904);
   text-align: center;
   margin: 10px 0px;
   padding: 4px 4px;
@@ -23,9 +25,11 @@ export const ArticleParagraph = styled.p`
 `
 
 export const ArticleImg = styled.div`
-  border: 1px solid black;
+  /* border: 1px solid black; */
+  padding: 10px;
   img {
     width: 100%;
     height: 100%;
+    border-radius: 5px;
   }
 `
--- client/src/components/AboutUs/styles.jsx
+++ client/src/components/AboutUs/styles.jsx
@@ -6,4 +6,5 @@ export const AboutUsWrapper = styled.div`
   grid-template-columns: 1fr 1fr 1fr;
   grid-column-gap: 5px;
   height: calc(100vh - 166px);
+  padding: 5px 0px;
 `
